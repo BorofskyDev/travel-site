e43f9dbe009731c3f845b577d44d39f5cb4a65f5
solovyovsdom <joelborofsky@mac.com>
2022-10-23 18:54:54 -0500

    updated links

--- data.js
+++ data.js
@@ -1,10 +1,9 @@
 export default [
   {
-    id:1,
+    id: 1,
     title: 'Kyiv',
     location: 'Ukraine',
-    googleMapsUrl:
-      'https://www.google.com/maps/place/Kyiv,+Ukraine,+02000/@50.401699,30.252512,10z/data=!3m1!4b1!4m5!3m4!1s0x40d4cf4ee15a4505:0x764931d2170146fe!8m2!3d50.4501!4d30.5234',
+    googleMapsUrl: 'https://goo.gl/maps/u4cyddak975BUWNg7',
     startDate: 'October, 2019',
     endDate: 'October, 2019',
     description:
@@ -12,11 +11,10 @@ export default [
     imageUrl: './src/assets/images/kyiv.jpeg',
   },
   {
-    id:2,
+    id: 2,
     title: 'Krakow',
     location: 'Poland',
-    googleMapsUrl:
-      'https://www.google.com/maps/place/Krak%C3%B3w,+Poland/@50.0464284,19.7246988,10z/data=!3m1!4b1!4m5!3m4!1s0x471644c0354e18d1:0xb46bb6b576478abf!8m2!3d50.0646501!4d19.9449799',
+    googleMapsUrl: 'https://goo.gl/maps/b73Mg2hAfSRKSPVq6',
     startDate: 'October, 2019',
     endDate: 'November, 2019',
     description:
@@ -24,11 +22,10 @@ export default [
     imageUrl: './src/assets/images/krakow.jpeg',
   },
   {
-    id:3,
+    id: 3,
     title: 'Birmingham/Cardiff',
     location: 'England/Wales',
-    googleMapsUrl:
-      'https://www.google.com/maps/place/Birmingham,+UK/@52.4970183,-2.1438044,10z/data=!3m1!4b1!4m5!3m4!1s0x4870942d1b417173:0xca81fef0aeee7998!8m2!3d52.486243!4d-1.890401',
+    googleMapsUrl: 'https://goo.gl/maps/SB7YS2hCctCWkijo6',
     startDate: 'October, 2017',
     endDate: 'October, 2017',
     description:
@@ -36,11 +33,10 @@ export default [
     imageUrl: './src/assets/images/birmingham.jpeg',
   },
   {
-    id:4,
+    id: 4,
     title: 'Zihuatanejo',
     location: 'Mexico',
-    googleMapsUrl:
-      'https://www.google.com/maps/place/Zihuatanejo,+Guerrero,+Mexico/@17.6405753,-101.5426272,14z/data=!3m1!4b1!4m5!3m4!1s0x8434771c57e1063b:0x24ed2d9d3ccc3530!8m2!3d17.6416693!4d-101.5516955',
+    googleMapsUrl: 'https://goo.gl/maps/D2hWAeuqXhiHGpRi8',
     startDate: 'July, 2010',
     endDate: 'January, 2020',
     description:
--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -5,23 +5,24 @@ import { ImLocation } from 'react-icons/im';
 function TravelCard(props) {
   return (
     <div className="card">
-      <img src={props.imageUrl} alt="" className="card__img" />
+      <img src={`${props.imageUrl}`} alt="" className="card__img" />
       <div className="card__container">
         <header className="card__header">
           <ImLocation className="card__icon" />
           <p className="card__country">{props.location}</p>
           <a
-            href="{props.googleMapsUrl}"
+            href={props.googleMapsUrl}
+            target="_blank"
             className="card__location-link"
           >
             View on Google Maps
           </a>
         </header>
         <h1 className="card__title">{props.title}</h1>
-        <p className="card__date">{props.startDate} - {props.endDate}</p>
-        <p className="card__description">
-          {props.description}
+        <p className="card__date">
+          {props.startDate} - {props.endDate}
         </p>
+        <p className="card__description">{props.description}</p>
       </div>
     </div>
   );
