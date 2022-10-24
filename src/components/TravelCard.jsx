91493d25ccb59a67ba9546e1c7704f6fdf68e45d
solovyovsdom <joelborofsky@mac.com>
2022-10-23 19:05:44 -0500

    final attempt at fixing deployment

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -4,7 +4,7 @@ import { ImLocation } from 'react-icons/im';
 function TravelCard(props) {
   return (
     <div className="card">
-      <img src={`../../src/assets/images/${props.imageUrl}`} alt="" className="card__img" />
+      <img src={`/src/assets/images/${props.imageUrl}`} alt="" className="card__img" />
       <div className="card__container">
         <header className="card__header">
           <ImLocation className="card__icon" />
